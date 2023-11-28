8462ff226a7045541b3e8ea257ef345ea12a63fd
Lup1in <gabrielcunhaleite@hotmail.com>
2023-11-28 03:36:48 -0300

    newpost css and jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import blogFetch from "../axios/config";
 import './Home.css'
 
 
@@ -8,7 +8,7 @@ const Home = () => {
     const [posts, setPosts] = useState([]);
     const getPosts = async () => {
         try {
-            const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+            const response = await blogFetch.get("/posts")
             const data = response.data;
             setPosts(data);
         } catch (e) {
@@ -19,14 +19,13 @@ const Home = () => {
         getPosts();
     }, [])
 
-    return ( <div>
+    return (<div className="home">
         <h1>Last Posts</h1>
-        {posts.length === 0 ? <p>Loading</p> :(
+        {posts.length === 0 ? <p>Loading</p> : (
             posts.map((post) => (
                 <div className="post" key={post.id}>
                     <h2>{post.title}</h2>
                     <p>{post.body}</p>
-                    <Link to={'/posts/${post.id}'} className="btn">See More</Link>
                 </div>
             ))
         )}
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,5 +1,39 @@
+import blogFetch from '../axios/config';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './NewPost.css'
+
 const newPost = () => {
-    return <div>newPost</div>
+    const navigate = useNavigate();
+    const [title, setTitle] = useState();
+    const [body, setBody] = useState();
+
+    const createPost = async (e) => {
+        e.preventDefault();
+
+        const post = {title, body, userId: 1};
+        await blogFetch.post("/posts", { 
+            body: post,
+        });
+        navigate("/");
+    };
+
+    return (
+        <div className="new-post">
+            <h2>New Post</h2>
+            <form onSubmit={(e) => createPost(e)}>
+                <div className="form-control">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" placeholder="type your title" id="title" onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="body">Content</label>
+                    <textarea name="body" placeholder="type your content" id="body" onChange={(e) => setBody(e.target.value)}></textarea>
+                </div>
+                <input type="submit" value="create post" className="btn" />
+            </form>
+        </div>)
 }
 
 export default newPost
